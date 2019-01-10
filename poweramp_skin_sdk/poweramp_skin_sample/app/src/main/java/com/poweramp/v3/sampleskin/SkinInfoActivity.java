a8a598f30561a12bef915a981eafbd15aa685719
maxmpz <maxmpz@gmail.com>
2019-01-10 21:28:40 +0300

    - added a small toast to the template activity (gcantoni)

--- poweramp_skin_sdk/poweramp_skin_sample/app/src/main/java/com/poweramp/v3/sampleskin/SkinInfoActivity.java
+++ poweramp_skin_sdk/poweramp_skin_sample/app/src/main/java/com/poweramp/v3/sampleskin/SkinInfoActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class SkinInfoActivity extends Activity {
 
@@ -18,6 +19,7 @@ public class SkinInfoActivity extends Activity {
         intent.putExtra("theme_pak", getPackageName());
         intent.putExtra("theme_id", R.style.SampleSkin);
         startActivity(intent);
+        //Toast.makeText(this, R.string.skin_applied_msg, Toast.LENGTH_LONG).show(); // Enable toast if needed
         finish();
     }
 
@@ -26,6 +28,7 @@ public class SkinInfoActivity extends Activity {
         intent.putExtra("theme_pak", getPackageName());
         intent.putExtra("theme_id", R.style.SampleSkinAAA);
         startActivity(intent);
+        //Toast.makeText(this, R.string.skin_applied_msg, Toast.LENGTH_LONG).show(); // Enable toast if needed
         finish();
     }
 
