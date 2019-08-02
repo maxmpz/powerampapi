bfde4d1439e87f7af24be30f9f1cab19eee7a619
maxmpz <maxmpz@gmail.com>
2019-08-02 19:20:47 +0300

    - gradle update - tabs - ids rebuild

--- poweramp_skin_sdk/poweramp_skin_sample/app/src/main/java/com/poweramp/v3/sampleskin/SkinInfoActivity.java
+++ poweramp_skin_sdk/poweramp_skin_sample/app/src/main/java/com/poweramp/v3/sampleskin/SkinInfoActivity.java
@@ -8,37 +8,37 @@ import android.widget.Toast;
 
 public class SkinInfoActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_skin_info);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_skin_info);
+	}
 
-    public void startWithSampleSkin(View view) {
-        Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.StartupActivity");
-        intent.putExtra("theme_pak", getPackageName());
-        intent.putExtra("theme_id", R.style.SampleSkin);
-        startActivity(intent);
-        //Toast.makeText(this, R.string.skin_applied_msg, Toast.LENGTH_LONG).show(); // Enable toast if needed
-        finish();
-    }
+	public void startWithSampleSkin(View view) {
+		Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.StartupActivity");
+		intent.putExtra("theme_pak", getPackageName());
+		intent.putExtra("theme_id", R.style.SampleSkin);
+		startActivity(intent);
+		//Toast.makeText(this, R.string.skin_applied_msg, Toast.LENGTH_LONG).show(); // Enable toast if needed
+		finish();
+	}
 
-    public void startWithSampleAAASkin(View view) {
-        Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.StartupActivity");
-        intent.putExtra("theme_pak", getPackageName());
-        intent.putExtra("theme_id", R.style.SampleSkinAAA);
-        startActivity(intent);
-        //Toast.makeText(this, R.string.skin_applied_msg, Toast.LENGTH_LONG).show(); // Enable toast if needed
-        finish();
-    }
+	public void startWithSampleAAASkin(View view) {
+		Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.StartupActivity");
+		intent.putExtra("theme_pak", getPackageName());
+		intent.putExtra("theme_id", R.style.SampleSkinAAA);
+		startActivity(intent);
+		//Toast.makeText(this, R.string.skin_applied_msg, Toast.LENGTH_LONG).show(); // Enable toast if needed
+		finish();
+	}
 
-    public void openPowerampThemeSettings(View view) {
-        Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.SettingsActivity");
-        intent.putExtra("open", "theme");
-        intent.putExtra("theme_pak", getPackageName()); // If theme_pak/theme_id specified for open/theme, will scroll to/opens this skins settings
-        intent.putExtra("theme_id", R.style.SampleSkin);
-        startActivity(intent);
-        finish();
-    }
+	public void openPowerampThemeSettings(View view) {
+		Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.SettingsActivity");
+		intent.putExtra("open", "theme");
+		intent.putExtra("theme_pak", getPackageName()); // If theme_pak/theme_id specified for open/theme, will scroll to/opens this skins settings
+		intent.putExtra("theme_id", R.style.SampleSkin);
+		startActivity(intent);
+		finish();
+	}
 
 }
