d743b5b025440488160171407a4506d7e29b8204
maxmpz <maxmpz@gmail.com>
2019-11-29 04:07:08 +0300

    - now supporting various Poweramp packages/editions

--- poweramp_skin_sdk/poweramp_skin_sample/app/src/main/java/com/poweramp/v3/sampleskin/SkinInfoActivity.java
+++ poweramp_skin_sdk/poweramp_skin_sample/app/src/main/java/com/poweramp/v3/sampleskin/SkinInfoActivity.java
@@ -1,12 +1,17 @@
 package com.poweramp.v3.sampleskin;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class SkinInfoActivity extends Activity {
+	private static final String TAG = "SkinInfoActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,29 +19,63 @@ public class SkinInfoActivity extends Activity {
 		setContentView(R.layout.activity_skin_info);
 	}
 
+	/**
+	 * @return resolved Poweramp package name or null if not installed
+	 * NOTE: can be called from any thread, though double initialization is possible, but it's OK
+	 */
+	public static String getPowerampPackageName(Context context) {
+		try {
+			ResolveInfo info = context.getPackageManager().resolveService(new Intent("com.maxmpz.audioplayer.API_COMMAND"), 0);
+			if(info != null && info.serviceInfo != null) {
+				return info.serviceInfo.packageName;
+			}
+		} catch(Throwable th) {
+			Log.e(TAG, "", th);
+		}
+		return null;
+	}
+
 	public void startWithSampleSkin(View view) {
-		Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.StartupActivity");
-		intent.putExtra("theme_pak", getPackageName());
-		intent.putExtra("theme_id", R.style.SampleSkin);
+		String pak = getPowerampPackageName(this);
+		if(pak == null) {
+			Toast.makeText(this, R.string.skin_poweramp_not_installed, Toast.LENGTH_LONG).show();
+			return;
+		}
+		Intent intent = new Intent(Intent.ACTION_MAIN)
+			.setClassName(pak, "com.maxmpz.audioplayer.StartupActivity")
+			.putExtra("theme_pak", getPackageName())
+			.putExtra("theme_id", R.style.SampleSkin);
 		startActivity(intent);
 		//Toast.makeText(this, R.string.skin_applied_msg, Toast.LENGTH_LONG).show(); // Enable toast if needed
 		finish();
 	}
 
 	public void startWithSampleAAASkin(View view) {
-		Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.StartupActivity");
-		intent.putExtra("theme_pak", getPackageName());
-		intent.putExtra("theme_id", R.style.SampleSkinAAA);
+		String pak = getPowerampPackageName(this);
+		if(pak == null) {
+			Toast.makeText(this, R.string.skin_poweramp_not_installed, Toast.LENGTH_LONG).show();
+			return;
+		}
+		Intent intent = new Intent(Intent.ACTION_MAIN)
+			.setClassName(pak, "com.maxmpz.audioplayer.StartupActivity")
+			.putExtra("theme_pak", getPackageName())
+			.putExtra("theme_id", R.style.SampleSkinAAA);
 		startActivity(intent);
 		//Toast.makeText(this, R.string.skin_applied_msg, Toast.LENGTH_LONG).show(); // Enable toast if needed
 		finish();
 	}
 
 	public void openPowerampThemeSettings(View view) {
-		Intent intent = new Intent(Intent.ACTION_MAIN).setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.SettingsActivity");
-		intent.putExtra("open", "theme");
-		intent.putExtra("theme_pak", getPackageName()); // If theme_pak/theme_id specified for open/theme, will scroll to/opens this skins settings
-		intent.putExtra("theme_id", R.style.SampleSkin);
+		String pak = getPowerampPackageName(this);
+		if(pak == null) {
+			Toast.makeText(this, R.string.skin_poweramp_not_installed, Toast.LENGTH_LONG).show();
+			return;
+		}
+		Intent intent = new Intent(Intent.ACTION_MAIN)
+			.setClassName(pak, "com.maxmpz.audioplayer.SettingsActivity")
+			.putExtra("open", "theme")
+			.putExtra("theme_pak", getPackageName()) // If theme_pak/theme_id specified for open/theme, will scroll to/opens this skins settings
+			.putExtra("theme_id", R.style.SampleSkin);
 		startActivity(intent);
 		finish();
 	}
