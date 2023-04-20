2799a4dc9d66ba243bc0f7f993a2de81fdbd208d
maxmpz <maxmpz@gmail.com>
2023-04-21 02:52:53 +0400

    -

--- poweramp_api_lib/src/main/java/com/maxmpz/poweramp/player/EqPresetConsts.java
+++ poweramp_api_lib/src/main/java/com/maxmpz/poweramp/player/EqPresetConsts.java
@@ -4,7 +4,15 @@ package com.maxmpz.poweramp.player;
  * @see TableDefs.EqPresets#TYPE
  */
 public interface EqPresetConsts {
+	/** Preset type not set */
+	public static final int TYPE_UNKNOWN = -1;
+
+	/** User created or default user preset */
 	public static final int TYPE_USER = 0;
+
+	/** Built-in preset */
 	public static final int TYPE_BUILT_IN = 10;
+
+	/** AutoEq preset */
 	public static final int TYPE_AUTO_EQ = 20;
 }
